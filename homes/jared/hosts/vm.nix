a598949b1527a8bc39532402c21393775d3168a3
sillock1 <jprest97@gmail.com>
2024-06-05 18:01:31 +0100

    chore: fix comment

--- homes/jared/hosts/vm.nix
+++ homes/jared/hosts/vm.nix
@@ -10,7 +10,7 @@
         # Configure your Display resolution, offset, scale and Monitors here, use `hyprctl monitors` to get the info.
         #   highres:      get the best possible resolution
         #   auto:         position automatically
-        #   1:          scale to 1.5 times
+        #   1.6:          scale to 1.6 times
         #   bitdepth,10:  enable 10 bit support
         monitor = "Virtual-1,highres,auto,1.6,bitdepth,10";
       };
