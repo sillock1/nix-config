255b1d0286ae354a28fc13acc6c5414c4e7a8722
sillock1 <jprest97@gmail.com>
2024-06-05 18:58:47 +0100

    feat: added gaming as module

--- homes/modules/desktop/base/default.nix
+++ homes/modules/desktop/base/default.nix
@@ -5,7 +5,6 @@
   imports = [
     ./home.nix
     ./anyrun.nix
-    ./games.nix
     ./media.nix
     ./xdg.nix
     ./browser.nix
--- homes/modules/desktop/default.nix
+++ homes/modules/desktop/default.nix
@@ -5,5 +5,6 @@
   imports = [
     ./base
     ./hyprland
+    ./gaming
   ];
 }
