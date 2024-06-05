dbb7e27b633054792949417faef9bc45a0fb205d
sillock1 <jprest97@gmail.com>
2024-06-05 17:43:52 +0100

    feat: added mkMerge for hyprland settings to pass custom monitor settings from host

--- homes/modules/desktop/hyprland/hyprland.nix
+++ homes/modules/desktop/hyprland/hyprland.nix
@@ -26,45 +26,52 @@ in
     };
   };
 
-  config = lib.mkIf cfg.enable {
-    systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
-    wayland.windowManager.hyprland = {
-      enable = true;
-      xwayland = {
+  config = lib.mkMerge [ 
+    (lib.mkIf cfg.enable {
+      systemd.user.targets.hyprland-session.Unit.Wants = [ "xdg-desktop-autostart.target" ];
+      wayland.windowManager.hyprland = {
         enable = true;
+        xwayland = {
+          enable = true;
+        };
+        settings = {
+          env = [
+            "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
+            "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
+            "MOZ_WEBRENDER,1"
+            "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+            "QT_QPA_PLATFORM,wayland"
+            "SDL_VIDEODRIVER,wayland"
+            "GDK_BACKEND,wayland"
+          ];
+        };
+        systemd.enable = true;
+        extraConfig = builtins.readFile ./conf/hyprland.conf;
       };
-      settings = {
-        env = [
-          "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
-          "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
-          "MOZ_WEBRENDER,1"
-          "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-          "QT_QPA_PLATFORM,wayland"
-          "SDL_VIDEODRIVER,wayland"
-          "GDK_BACKEND,wayland"
-        ];
+      xdg.configFile = {
+        "hypr/mako" = {
+          source = ./conf/mako;
+          recursive = true;
+        };
+        "hypr/scripts" = {
+          source = ./conf/scripts;
+          recursive = true;
+          executable = true;
+        };
+        "hypr/waybar" = {
+          source = ./conf/waybar;
+          recursive = true;
+        };
+        "hypr/wlogout" = {
+          source = ./conf/wlogout;
+          recursive = true;
+        };
       };
-      systemd.enable = true;
-      extraConfig = builtins.readFile ./conf/hyprland.conf;
-    };
-    xdg.configFile = {
-      "hypr/mako" = {
-        source = ./conf/mako;
-        recursive = true;
+    })
+    (lib.mkIf cfg.enable {
+      wayland.windowManager.hyprland = {
+        settings = cfg.settings;
       };
-      "hypr/scripts" = {
-        source = ./conf/scripts;
-        recursive = true;
-        executable = true;
-      };
-      "hypr/waybar" = {
-        source = ./conf/waybar;
-        recursive = true;
-      };
-      "hypr/wlogout" = {
-        source = ./conf/wlogout;
-        recursive = true;
-      };
-    };
-  };
+    })
+  ];
 }
