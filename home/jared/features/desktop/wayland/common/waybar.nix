47d290e908145e6f99ed2b0496ff3135c090827f
sillock1 <jprest97@gmail.com>
2024-07-11 12:09:26 +0100

    chore: run waybar from hyprland

--- home/jared/features/desktop/wayland/common/waybar.nix
+++ home/jared/features/desktop/wayland/common/waybar.nix
@@ -7,15 +7,11 @@
   ...
 }: 
 {
-  systemd.user.services.waybar = {
-    Unit.StartLimitBurst = 30;
-  };
   programs.waybar = {
     enable = true;
     package = pkgs.waybar.overrideAttrs (oa: {
       mesonFlags = (oa.mesonFlags or []) ++ ["-Dexperimental=true"];
     });
-    systemd.enable = true;
   };
 
   xdg.configFile."waybar" = {
