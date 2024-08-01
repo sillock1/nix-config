c112edeae89e1ac60d5174a6b48f9ae2bffbd237
sillock1 <mail@sillock.com>
2024-08-01 16:15:21 +0100

    chore: added extra config for monitors in hyprland

--- home/jared/features/desktop/wayland/hyprland/default.nix
+++ home/jared/features/desktop/wayland/hyprland/default.nix
@@ -3,48 +3,83 @@
   config,
   pkgs,
   ...
-}: {
+}: 
+let
+  cfg = config.modules.desktop.hyprland;
+in
+{
+  options.modules.desktop.hyprland = {
+    enable = lib.mkEnableOption "hyprland";
+    settings = lib.mkOption {
+      type = with lib.types; let
+        valueType =
+          nullOr (oneOf [
+            bool
+            int
+            float
+            str
+            path
+            (attrsOf valueType)
+            (listOf valueType)
+          ])
+          // {
+            description = "Hyprland configuration value";
+          };
+      in
+        valueType;
+      default = {};
+    };
+  };
   imports = [
     ../../common
     ../common
   ];
 
-  xdg.portal = let
-    hyprland = config.wayland.windowManager.hyprland.package;
-    xdph = pkgs.xdg-desktop-portal-hyprland.override {inherit hyprland;};
-  in {
-    extraPortals = [xdph];
-    configPackages = [hyprland];
-  };
+  config = lib.mkMerge [
+    ({
+      xdg.portal = let
+        hyprland = config.wayland.windowManager.hyprland.package;
+        xdph = pkgs.xdg-desktop-portal-hyprland.override {inherit hyprland;};
+      in {
+        extraPortals = [xdph];
+        configPackages = [hyprland];
+      };
 
-  home.packages = with pkgs; [
-    grimblast
-    hyprpicker
-  ];
-
-  wayland.windowManager.hyprland = {
-    enable = true;
-    xwayland.enable = true;
-    package = pkgs.hyprland.override {wrapRuntimeDeps = false;};
-    systemd = {
-      enable = true;
-      # Same as default, but stop graphical-session too
-      extraCommands = lib.mkBefore [
-        "systemctl --user stop graphical-session.target"
-        "systemctl --user start hyprland-session.target"
+      home.packages = with pkgs; [
+        grimblast
+        hyprpicker
       ];
-    };
-    settings = {
-      env = [
-        "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
-        "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
-        "MOZ_WEBRENDER,1"
-        "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
-        "QT_QPA_PLATFORM,wayland"
-        "SDL_VIDEODRIVER,wayland"
-        "GDK_BACKEND,wayland"
-      ];
-    };
-    extraConfig = builtins.readFile ../../../../config/hyprland/hyprland.conf;
-  };
+      wayland.windowManager.hyprland = {
+        enable = true;
+        xwayland.enable = true;
+        package = pkgs.hyprland.override {wrapRuntimeDeps = false;};
+        systemd = {
+          enable = true;
+          # Same as default, but stop graphical-session too
+          extraCommands = lib.mkBefore [
+            "systemctl --user stop graphical-session.target"
+            "systemctl --user start hyprland-session.target"
+          ];
+        };
+        settings = {
+          env = [
+            "NIXOS_OZONE_WL,1" # for any ozone-based browser & electron apps to run on wayland
+            "MOZ_ENABLE_WAYLAND,1" # for firefox to run on wayland
+            "MOZ_WEBRENDER,1"
+            "QT_WAYLAND_DISABLE_WINDOWDECORATION,1"
+            "QT_QPA_PLATFORM,wayland"
+            "SDL_VIDEODRIVER,wayland"
+            "GDK_BACKEND,wayland"
+          ];
+        };
+        extraConfig = builtins.readFile ../../../../config/hyprland/hyprland.conf;
+        };
+    })
+    (lib.mkIf cfg.enable {
+      wayland.windowManager.hyprland = {
+        settings = cfg.settings;
+      };
+    })
+   ];
+  
 }
--- home/jared/hosts/luna.nix
+++ home/jared/hosts/luna.nix
@@ -1,11 +1,26 @@
 { config, pkgs, lib, ...}:
 {
-imports = [
+  imports = [
     ../global
     ../features/desktop/wayland/hyprland
     ../features/development
     ../features/games
   ];
+
+  modules = {
+    desktop = {
+      hyprland = {
+        enable = true;
+        settings = {
+          monitor = [
+            "HDMI-A-1,2560x1440, 0x0, 1"
+            "DP-2,2560x1440, 2560x0, 1"
+            "DP-1,2560x1440, 5120x0, 1"
+          ];
+        };
+      };
+    };
+  };
 
   monitors = [
     {
