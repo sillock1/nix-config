f9b8d5d4b83afb917cb7bd88941ab4cb3caaecd1
sillock1 <mail@sillock.com>
2024-08-03 20:08:39 +0100

    chore: impermanence for lutris

--- home/jared/features/games/lutris.nix
+++ home/jared/features/games/lutris.nix
@@ -4,21 +4,16 @@
   ...
 }:
 {
-  imports = [
-    ./steam.nix
-    ./prism-launcher.nix
-    ./lutris.nix
-  ];
-
   home = {
     packages = with pkgs; [
-      gamescope
+      lutris
+      wineWowPackages.stable
     ];
     persistence = {
       "/persist/home/${config.home.username}" = {
         allowOther = true;
         directories = [
-          "games"
+          ".local/share/lutris"
         ];
       };
     };
