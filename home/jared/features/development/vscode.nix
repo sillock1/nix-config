fc56095cc57449b642ffe07edb5716773f2cfb4f
jared <git@sillock.io>
2024-08-20 11:21:57 +0100

    chore: remove vscode vim

--- home/jared/features/development/vscode.nix
+++ home/jared/features/development/vscode.nix
@@ -8,7 +8,6 @@
     enable = true;
     userSettings = lib.importJSON ../../config/editor/vscode/settings.json;
     extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
       jnoortheen.nix-ide
     ];
   };
