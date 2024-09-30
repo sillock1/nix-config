f9a111616e685161e4f551c226399a9d880b5fef
sillock <mail@sillock.com>
2024-09-30 12:20:24 +0100

    feat: extra disk for luna

--- hosts/luna/hardware-configuration.nix
+++ hosts/luna/hardware-configuration.nix
@@ -1,7 +1,6 @@
 {
   fileSystems."/nix".neededForBoot = true;
   fileSystems."/persist/system".neededForBoot = true;
-  fileSystems."/games" = {
-    device = "/dev/disk/by-id/wwn-0x500a0751e4e02e08-part1";
-  };
+  fileSystems."/games".device = "/dev/disk/by-id/wwn-0x500a0751e4e02e08-part1";
+  fileSystems."/bigfiles".device = "/dev/disk/by-id/wwn-0x5002538d41aea481-part1";
 }
