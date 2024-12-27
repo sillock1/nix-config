c593ddfb04bb62ba16529398dc9f7c0d5484daf8
sillock <mail@sillock.com>
2024-12-27 17:21:38 +0000

    chore: remove ansible and added rust analyzer vs code extension

--- home/jared/features/development/vscode.nix
+++ home/jared/features/development/vscode.nix
@@ -9,6 +9,7 @@
     userSettings = lib.importJSON ../../config/editor/vscode/settings.json;
     extensions = with pkgs.vscode-extensions; [
       jnoortheen.nix-ide
+      rust-lang.rust-analyzer
     ];
   };
 }
