6f85c07fd6620b87d4f6b001448c2f4e3ebfe1f8
jared <git@sillock.io>
2025-01-08 21:16:36 +0000

    feat: bump dotnet version to 9

--- home/jared/features/development/dotnet.nix
+++ home/jared/features/development/dotnet.nix
@@ -7,12 +7,12 @@
 
   home = {
     packages = with pkgs; [
-      dotnet-sdk_8
+      dotnet-sdk_9
     ];
     sessionPath = [
       "$HOME/.dotnet/tools"
     ];
-    sessionVariables.DOTNET_ROOT = "${pkgs.dotnet-sdk_8}";
+    sessionVariables.DOTNET_ROOT = "${pkgs.dotnet-sdk_9}";
 
     persistence."/persist/home/${config.home.username}" = {
       directories = [".microsoft/usersecrets"];
