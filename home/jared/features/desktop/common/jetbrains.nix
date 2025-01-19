ee47b4f88a4c8f08ab029862705db3e84fb7a9db
jared <git@sillock.io>
2025-01-19 13:59:00 +0000

    feat: add jetbrains packages directly instead of toolbox

--- home/jared/features/desktop/common/jetbrains.nix
+++ home/jared/features/desktop/common/jetbrains.nix
@@ -6,15 +6,16 @@
 {
   home = {
     packages = with pkgs; [
-      unstable.jetbrains-toolbox
+      unstable.jetbrains.rider
+      unstable.jetbrains.datagrip
+      unstable.jetbrains.idea-ultimate
     ];
     persistence = {
       "/persist/home/${config.home.username}" = {
         allowOther = true;
         directories = [
+          ".config/JetBrains"
           ".local/share/JetBrains"
-          "RiderProjects"
-          "DataGripProjects"
         ];
       };
     };
