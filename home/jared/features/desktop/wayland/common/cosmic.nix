6e1d4a1022b96d993913718bb1d67a6e5d52ac3d
sillock <mail@sillock.com>
2025-05-24 16:54:46 +0100

    fix: add xdg cosmic portal

--- home/jared/features/desktop/wayland/common/cosmic.nix
+++ home/jared/features/desktop/wayland/common/cosmic.nix
@@ -1,8 +1,11 @@
 {
   config,
+  pkgs,
   ...
 }:
 {
+  xdg.portal.extraPortals = with pkgs; [xdg-desktop-portal-cosmic];
+
   home = {
     persistence = {
       "/persist/home/${config.home.username}" = {
